70f3bead387a31011c9c186426e2f69561d04710
Florêncio Gonçalves <109610920+florencioGoncalves@users.noreply.github.com>
2023-06-10 01:17:07 +0100

    First commit | Cabeçalho feito

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppHeader />
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style lang="scss" src="./assets/style/global.scss">
+</style>
